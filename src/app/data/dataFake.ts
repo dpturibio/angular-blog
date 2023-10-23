fecabd329f758b4a0c349d341b331e53a9dabe4c
Diego Prosperi Turibio <diegoprosperituribio@gmail.com>
2023-10-23 20:29:14 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,30 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "photo":"aaa",
+        "title":"saiu novo filme",
+        "description": "description of movie 1",
+        "photoCover": "../../../photos/1.jpg"
+    },
+    {
+        "id":"2",
+        "photo":"bbb",
+        "title":"saiu novo filme",
+        "description": "description of movie 2",
+        "photoCover": "../../../photos/2.jpg"
+    },
+    {
+        "id":"3",
+        "photo":"ccc",
+        "title":"saiu novo filme",
+        "description": "description of movie 3",
+        "photoCover": "../../../photos/3.png"
+    },
+    {
+        "id":"4",
+        "photo":"ddd",
+        "title":"saiu novo filme",
+        "description": "description of movie 4",
+        "photoCover": "../../../photos/bec.jpeg"
+    },
+]
